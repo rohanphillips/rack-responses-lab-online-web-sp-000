2ba9b5f201f85a298502320f4b057ccda4d36fd4
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-27 13:59:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    t = Time.now.strftime("%H")
-    z = t.to_i < 12
-    resp.write(z)
-    #Time.now.strftime("%H") < 12 ? resp.write("Good Morning!") : resp.write("Good Afternoon")
+    Time.now.strftime("%H").to_i < 12 ? resp.write("Good Morning!") : resp.write("Good Afternoon")
     resp.finish
   end
 end
